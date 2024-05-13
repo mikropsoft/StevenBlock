8944d4c76028e83aefe43a1e503c40b90bc31e29
W͏ I͏ N͏ Z͏ O͏ R͏ T͏ <75412448+mikropsoft@users.noreply.github.com>
2024-05-13 08:45:21 +0300

    Add files via upload

--- customize.sh
+++ customize.sh
@@ -34,10 +34,6 @@ REPLACE_EXAMPLE="
 
 # Construct your own list here
 REPLACE="
-/system/etc/hosts
-/system/etc/hosts.tmp
-/system/bin/mv
-/system/bin/sed
 "
 
 ##########################################################################################
@@ -45,12 +41,24 @@ REPLACE="
 ##########################################################################################
 
 set_permissions() {
-  :
-  set_perm_recursive $MODPATH/system/etc/hosts 0 0 0644 0755
-  set_perm  $MODPATH/system/bin/stevenblock  0  0  0777
-  set_perm  $MODPATH/system/bin/curl  0  0  0777
-  set_perm  $MODPATH/system/bin/mv  0  0  0777
-  set_perm  $MODPATH/system/bin/sed  0  0  0777
+  : # Remove this if adding to this function
+  
+  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
+  # Some examples:
+  
+  # For directories (includes files in them):
+  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
+  set_perm  $MODPATH/system/bin/add  0  0  0777
+  set_perm  $MODPATH/system/bin/remove  0  0  0777
+  set_perm  $MODPATH/system/bin/updater  0  0  0777
+  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
+  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
+
+  # For files (not in directories taken care of above)
+  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
+  
+  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
+  # set_perm /data/local/tmp/file.txt 0 0 644
 }
 
 ##########################################################################################
