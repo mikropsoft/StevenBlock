3f2026d437bf6562df372cf216f6b54450a93ae8
𝗦𝗵𝗟𝗲𝗿𝗣 <75412448+mikropsoft@users.noreply.github.com>
2024-09-10 18:46:08 +0300

    Add files via upload

--- common/functions.sh
+++ common/functions.sh
@@ -160,6 +160,8 @@ ui_print "
 * Note: Please refrain from using this module in conjunction with the AdAway application and the systemless hosts module.
 
 * Note: If you're experiencing issues with the StevenBlack hosts list, try the 1Hosts version.
+
+* Note: The 1hosts list hasn't been updated recently, so if the version is still the same, do not attempt to update by installing the same zip file.
 "
 
 # Check for min/max api version
